773a5efa07b23b7c340368d3cb27878c4cedb8ab
Johan Thureson <johan.thureson@gmail.com>
2024-01-06 11:38:48 +0100

    ArtistListView

--- Discogs/ArtistListView.swift
+++ Discogs/ArtistListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ArtistListView.swift
 //  Discogs
 //
 //  Created by Johan Thureson on 2024-01-06.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ArtistListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ArtistListView()
 }
