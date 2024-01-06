9681a60d09efa80de3431a66fdf42f66b1b41b46
Johan Thureson <johan.thureson@gmail.com>
2024-01-06 11:36:13 +0100

    Initial Commit

---
+++ Discogs/DiscogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscogsApp.swift
+//  Discogs
+//
+//  Created by Johan Thureson on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct DiscogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
