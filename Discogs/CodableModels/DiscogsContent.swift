86e7d97f9f45cebf0c33644e680a4cf16b18d73e
Johan Thureson <johan.thureson@gmail.com>
2024-01-07 19:21:16 +0100

    updated models

--- Discogs/CodableModels/DiscogsContent.swift
+++ Discogs/CodableModels/DiscogsContent.swift
@@ -1,15 +1,14 @@
 import Foundation
 
-struct DiscogsContent : Codable {
-	let releases : [Releases]?
+public struct DiscogsContent : Codable {
+    
+    public let releases : [Releases]?
 
-	enum CodingKeys: String, CodingKey {
-		case releases = "releases"
-	}
+    enum CodingKeys: String, CodingKey {
+        case releases = "releases"
+    }
 
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		releases = try values.decodeIfPresent([Releases].self, forKey: .releases)
-	}
-
+    public init(releases: [Releases]?) {
+        self.releases = releases
+    }
 }
--- Discogs/CodableModels/Releases.swift
+++ Discogs/CodableModels/Releases.swift
@@ -1,48 +1,75 @@
 import Foundation
 
-struct Releases : Codable, Identifiable {
-	let id : Int?
-	let status : String?
-	let type : String?
-	let format : String?
-	let label : String?
-	let title : String?
-	let resource_url : String?
-	let role : String?
-	let artist : String?
-	let year : Int?
-	let thumb : String?
-	let stats : Stats?
-
-	enum CodingKeys: String, CodingKey {
-		case id = "id"
-		case status = "status"
-		case type = "type"
-		case format = "format"
-		case label = "label"
-		case title = "title"
-		case resource_url = "resource_url"
-		case role = "role"
-		case artist = "artist"
-		case year = "year"
-		case thumb = "thumb"
-		case stats = "stats"
-	}
-
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		id = try values.decodeIfPresent(Int.self, forKey: .id)
-		status = try values.decodeIfPresent(String.self, forKey: .status)
-		type = try values.decodeIfPresent(String.self, forKey: .type)
-		format = try values.decodeIfPresent(String.self, forKey: .format)
-		label = try values.decodeIfPresent(String.self, forKey: .label)
-		title = try values.decodeIfPresent(String.self, forKey: .title)
-		resource_url = try values.decodeIfPresent(String.self, forKey: .resource_url)
-		role = try values.decodeIfPresent(String.self, forKey: .role)
-		artist = try values.decodeIfPresent(String.self, forKey: .artist)
-		year = try values.decodeIfPresent(Int.self, forKey: .year)
-		thumb = try values.decodeIfPresent(String.self, forKey: .thumb)
-		stats = try values.decodeIfPresent(Stats.self, forKey: .stats)
-	}
-
+public struct Releases: Codable, Equatable, Identifiable, Hashable {
+    
+    public let id : Int?
+    public let status : String?
+    public let type : String?
+    public let format : String?
+    public let label : String?
+    public let title : String?
+    public let resource_url : String?
+    public let role : String?
+    public let artist : String?
+    public let year : Int?
+    public let thumb : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case status = "status"
+        case type = "type"
+        case format = "format"
+        case label = "label"
+        case title = "title"
+        case resource_url = "resource_url"
+        case role = "role"
+        case artist = "artist"
+        case year = "year"
+        case thumb = "thumb"
+    }
+    
+    public init(
+        id: Int?,
+        status: String?,
+        type: String?,
+        format: String?,
+        label: String?,
+        title: String?,
+        resource_url: String?,
+        role: String?,
+        artist: String?,
+        year: Int?,
+        thumb: String?
+    ) {
+        self.id = id
+        self.status = status
+        self.type = type
+        self.format = format
+        self.label = label
+        self.title = title
+        self.resource_url = resource_url
+        self.role = role
+        self.artist = artist
+        self.year = year
+        self.thumb = thumb
+    }
+    
+    public static func == (lhs: Releases, rhs: Releases) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    public static func sample(id: Int = 999, artist: String = "Speak & Spell") -> Releases {
+        Releases(id: id,
+                 status: "",
+                 type: "Record",
+                 format: "LP",
+                 label: "Mute",
+                 title: "Speak & Spell",
+                 resource_url: "",
+                 role: "",
+                 artist: "Depeche Mode",
+                 year: 1981,
+                 thumb: ""
+        )
+    }
 }
