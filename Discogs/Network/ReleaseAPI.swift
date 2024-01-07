f6022ba517319ec081e8d83e4fc8703890e57473
Johan Thureson <johan.thureson@gmail.com>
2024-01-07 21:01:53 +0100

    .

--- Discogs/Network/ReleaseAPI.swift
+++ Discogs/Network/ReleaseAPI.swift
@@ -67,7 +67,6 @@ public final class ReleaseAPIImpl: ReleaseAPI {
                     where error.domain == NSURLErrorDomain
                     && error.code == NSURLErrorNotConnectedToInternet {
             throw ReleaseAPIError.offline
-            
         } catch {
             throw error
         }
